011a06d8804c579e8708e436919a8bf76902a7eb
Aditya Keshari <68159582+aditya292002@users.noreply.github.com>
2022-09-02 23:36:53 +0530

    Add files via upload

---
+++ helpful_packages.sh
@@ -0,0 +1,18 @@
+sudo apt install snap
+echo "Done"
+sudo apt install snapd
+echo "Done"
+sudo apt install guake
+echo "Done"
+sudo apt install htop
+echo "Done"
+sudo snap install bpytop
+echo "Done"
+sudo apt install speedtest-cli
+echo "Done"
+sudo apt install lm-sensors
+echo "Done"
+sudo apt install mlocate
+echo "Done"
+sudo apt install ranger
+echo "Done"
